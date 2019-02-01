dc75290574ecef7ab3af5be67c7dba0c50150412
Marcos Vinícius <marcos.amaral@radixeng.com.br>
2019-02-01 21:29:38 -0200

    Adicionando redux thunk

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './store/reducer';
+import thunk from 'redux-thunk'
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
