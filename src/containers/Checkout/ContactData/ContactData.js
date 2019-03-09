9e308db4d260fb95834b143ffddfb859cea883e2
Marcos Souza <markin.mv.amaral@gmail.com>
2019-03-09 19:53:22 -0300

    Filtro de pedidos e salvando token usuário

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         };
 
         this.props.onOrderBurger(order, this.props.token);
@@ -201,6 +202,7 @@ const mapStateToProps = state => {
         price: state.burgerBuilder.totalPrice,
         state: state.order.loading,
         token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
