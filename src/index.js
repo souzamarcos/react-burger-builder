054fe761bb2e583b9124ba4f9512d6491fe56714
Marcos Amaral <marcos.amaral@radixeng.com.br>
2019-04-12 17:02:16 -0300

    Adicionando redux saga

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth } from './store/sagas/index'; 
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null|| compose;
 
@@ -19,7 +21,11 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunk/*, sagaMiddleware*/)));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
